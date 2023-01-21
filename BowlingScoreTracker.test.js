c17cd469aa355f476cc17b2a45e743dc30353856
Ashjag47 <Ashish_P@mckinsey.com>
2023-01-21 21:35:51 +0530

    Test: add test caese to check all possible invalid cases

--- BowlingScoreTracker.test.js
+++ BowlingScoreTracker.test.js
@@ -1,10 +1,16 @@
-const calculateTotalScore = require('./BowlingScoreTracker');
+const calculateTotalScore = require("./BowlingScoreTracker");
 
 const invalidCases = [
-    [["a",2,3],"Invalid input"],
-    [["a",'.',3],"Invalid input"]
-]
+  [["a", 2, 3], "Invalid input"],
+  [["a", ".", 3], "Invalid input"],
+  [[3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 5], "Invalid input"],
+  [[3, 6, 3, 7, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3], "Invalid input"],
+  [[3, 6, 3, 7, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 7, 3, 9], "Invalid input"],
+  [[3, 6, 3, 7, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 7, 10, 1], "Invalid input"],
+  [[3, 6, 3, 7, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 10, 3, 7, 7], "Invalid input"],
+  [[3, 6, 3, 7, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 10, 10, 9, 1], "Invalid input"]
+];
 
-test.each(invalidCases)('%p is %s', (input,output) =>{
-    expect(calculateTotalScore(input)).toBe(output);
+test.each(invalidCases)("%p is %s", (input, output) => {
+  expect(calculateTotalScore(input)).toBe(output);
 });
